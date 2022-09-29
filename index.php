87eda416009b8006acc797e01e7553b90f6ece92
Jake Schmidt <nl6701cy@go.minnstate.edu>
2022-09-29 17:58:01 -0500

    Added select box back for list and grid view

--- index.php
+++ index.php
@@ -1,14 +1,3 @@
-<?php
-
-session_start();
-
-    include("connection.php");
-    include("functions.php");
-
-    $user_data = check_login($con);
-	
-?>
-
 <!DOCTYPE html>
 <html lang="en" style="background-color: #1c1c1c">
 <head>
@@ -67,6 +56,10 @@ session_start();
    aria-label="Search through site content">
   <button class="search-button"><svg viewBox="0 0 1024 1024"><path class="path1" d="M848.471 928l-263.059-263.059c-48.941 36.706-110.118 55.059-177.412 55.059-171.294 0-312-140.706-312-312s140.706-312 312-312c171.294 0 312 140.706 312 312 0 67.294-24.471 128.471-55.059 177.412l263.059 263.059-79.529 79.529zM189.623 408.078c0 121.364 97.091 218.455 218.455 218.455s218.455-97.091 218.455-218.455c0-121.364-103.159-218.455-218.455-218.455-121.364 0-218.455 97.091-218.455 218.455z"></path></svg></button>
 </form>
+<select class="home-select">
+  <option value="List View" selected>List View</option>
+  <option value="Grid View">Grid View</option>
+</select>
         </div>
         <a href="login.php">
         <button type="button" class="home-button button" class="login-button">Login</button></a>
