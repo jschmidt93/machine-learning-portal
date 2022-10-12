83c15c0a2455849a6266f589c4cc8252445a402b
AlexanderTorres3 <AlexanderTorres3@outlook.com>
2022-10-11 19:31:29 -0500

    fixed sql

--- machine-learning-portal.sql
+++ machine-learning-portal.sql
@@ -28,10 +28,10 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `instructor` (
-  `userID` int(11) DEFAULT NULL,
+  `instructor_id` int(11) DEFAULT NULL,
   `username` varchar(55) DEFAULT NULL,
-  `userPassWord` varchar(100) DEFAULT NULL,
-  `displayName` varchar(100) DEFAULT NULL,
+  `password` varchar(100) DEFAULT NULL,
+  `display_name` varchar(100) DEFAULT NULL,
   `permissions` varchar(100) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -39,7 +39,7 @@ CREATE TABLE `instructor` (
 -- Dumping data for table `instructor`
 --
 
-INSERT INTO `instructor` (`userID`, `username`, `userPassWord`, `displayName`, `permissions`) VALUES
+INSERT INTO `instructor` (`instructor_id`, `username`, `password`, `display_name`, `permissions`) VALUES
 (1, 'MacJones', 'Pats', 'Mac', 'All'),
 (2, 'JoeyB', 'Cinci', 'Joe', 'All'),
 (3, 'KirkC', 'trash', 'Kirk', 'None'),
@@ -53,33 +53,34 @@ INSERT INTO `instructor` (`userID`, `username`, `userPassWord`, `displayName`, `
 --
 
 CREATE TABLE `resources` (
-  `id` int(11) NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT ,
   `topics` varchar(100) DEFAULT NULL,
   `description` varchar(2000) DEFAULT NULL,
   `type` varchar(255) DEFAULT NULL,
   `keywords` varchar(20) DEFAULT NULL,
   `link` varchar(500) DEFAULT NULL,
-  `user_id` varchar(20) DEFAULT NULL
+  `instructor_id` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `resources`
 --
 
-INSERT INTO `resources` (`id`, `topics`, `description`, `type`, `keywords`, `link`, `user_id`) VALUES
-(1, 'Gaussian Processes', 'a generic supervised learning method designed to solve regression and probabilistic classification problems.', 
+INSERT INTO `resources` (`id`, `topics`, `description`, `type`, `keywords`, `link`, `instructor_id`) VALUES
+(NULL, 'Gaussian Processes', 'a generic supervised learning method designed to solve regression and probabilistic classification problems.', 
 'Supervised Learning', 'GPR', 'https://scikit-learn.org/stable/modules/gaussian_process.html#:~:text=Gaussian%20Processes%20(GP)%20are%20a,at%20least%20for%20regular%20kernels).', '1'),
-(2, 'Naive Bayes', 'Is a classification algorithm that uses Bayes theorem', 'Supervised Learning', 
+(NULL, 'Naive Bayes', 'Is a classification algorithm that uses Bayes theorem', 'Supervised Learning', 
 'Bayes', 'https://scikit-learn.org/stable/modules/naive_bayes.html', '2'),
-(3, 'Cross decomposition', 'Is an algorithm that will find the similaries between two matrices', 'Supervised Learning', 'matrices', 'https://scikit-learn.org/stable/modules/cross_decomposition.html', '3'),
-(4, 'Decision Trees', 'Are used for classicfication and regression methods', 'Supervised Learning ', 'Tree', 'https://scikit-learn.org/stable/modules/tree.html', '4'),
-(5, 'Clustering', 'These algorithms have two parts one that trains the data. The other that trains the data', 
+(NULL, 'Cross decomposition', 'Is an algorithm that will find the similaries between two matrices', 'Supervised Learning', 'matrices', 'https://scikit-learn.org/stable/modules/cross_decomposition.html', '3'),
+(NULL, 'Decision Trees', 'Are used for classicfication and regression methods', 'Supervised Learning ', 'Tree', 'https://scikit-learn.org/stable/modules/tree.html', '4'),
+(NULL, 'Clustering', 'These algorithms have two parts one that trains the data. The other that trains the data', 
 'Unsupervised Learning', 'Unsupervised Learning', 'https://scikit-learn.org/stable/modules/clustering.html', '5'),
-(6, 'Manifold learning', 'Data in these sets take a non leaner approach', 'Unsupervised Learning', 'Unsupervised Learning', 'https://scikit-learn.org/stable/modules/manifold.html', '6'),
-(7, 'Gaussian mixture models', 'Is a model that takes into account the possible of mixted finite numbers for data sets. It generates their proablilty ', 'Unsupervised learning', 'Unsupervised learning, Gaussian', 'https://www.walmart.com/', '7'),
-(8, 'Biclustering', 'It will cluster rows and columns of matrixs', 'Unsupervised learning ', 'Unsupervised learning, clustering', 'https://www.miamidolphins.com/', '8'),
-(9, 'Density Estimation', 'Can use models like Gaussian Mixtures or kernel density. To estimate the density of data sets.', 'Unsupervised learning', 'Unsupervised learning', 'https://scikit-learn.org/stable/modules/density.html', '9'),
-(10, 'Neural network models', 'Restricted Boltzmann machines are one kind of neural network mdoels. This model assumes inputs are binary.This data would be encoded.',
+(NULL, 'Manifold learning', 'Data in these sets take a non leaner approach', 'Unsupervised Learning', 'Unsupervised Learning', 'https://scikit-learn.org/stable/modules/manifold.html', '6'),
+(NULL, 'Gaussian mixture models', 'Is a model that takes into account the possible of mixted finite numbers for data sets. It generates their proablilty ', 'Unsupervised learning', 'Unsupervised learning, Gaussian', 'https://www.walmart.com/', '7'),
+(NULL, 'Biclustering', 'It will cluster rows and columns of matrixs', 'Unsupervised learning ', 'Unsupervised learning, clustering', 'https://www.miamidolphins.com/', '8'),
+(NULL, 'Density Estimation', 'Can use models like Gaussian Mixtures or kernel density. To estimate the density of data sets.', 'Unsupervised learning', 'Unsupervised learning', 'https://scikit-learn.org/stable/modules/density.html', '9'),
+(NULL, 'Neural network models', 'Restricted Boltzmann machines are one kind of neural network mdoels. This model assumes inputs are binary.This data would be encoded.',
  'Unsupervised learning', 'Unsupervised learning, Neural networks', 'https://scikit-learn.org/stable/modules/neural_networks_unsupervised.html', '10');
 
 --
@@ -89,9 +90,9 @@ INSERT INTO `resources` (`id`, `topics`, `description`, `type`, `keywords`, `lin
 --
 -- Indexes for table `resources`
 --
-ALTER TABLE `resources`
-  ADD PRIMARY KEY (`id`);
-COMMIT;
+-- ALTER TABLE `resources`
+--   ADD PRIMARY KEY (`id`);
+-- COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
